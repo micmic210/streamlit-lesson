27d79ccd9dc81d713412cd27e0cd5bbb7f14c204
Michiko  Inoue <inouem888@gmail.com>
2024-12-16 11:24:35 +0000

    Add calculator.py

--- 3_calculator.py
+++ 3_calculator.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from app_pages.multi_page import MultiPage
 
+from app_pages.calculator import calculator_body
+
+app = MultiPage(app_name = "Ca;ci;atpr App")
+
+app.add_page("Calculator", calculator_body)
+
+app.run()
